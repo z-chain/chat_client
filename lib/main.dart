10c08c77c33425b72bd11c631e995e76ad553ed6
iamee <i@iamee.me>
2022-01-23 15:37:36 +0800

    增加客户端支持

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,24 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
+import 'package:window_manager/window_manager.dart';
 
 import 'app/index.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isMacOS || Platform.isLinux || Platform.isWindows) {
+    await windowManager.ensureInitialized();
+    windowManager.waitUntilReadyToShow().then((_) async {
+      await windowManager.setAsFrameless();
+      await windowManager.setSize(const Size(300, 500));
+      windowManager.show();
+    });
+  }
 
   final preferences = await SharedPreferences.getInstance();
 
